67d9de067dd447125b570d40bf814600c1c2288f
Syed Fasiuddin <syed.fasiuddin@icloud.com>
2025-01-28 13:45:43 +0530

    send & recv ICMP packet

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,74 @@
-type HModule = *const std::ffi::c_void;
-type FarProc = *const std::ffi::c_void;
+use std::ffi::c_void;
+use std::fmt;
+use std::mem;
+
+type HModule = *const c_void;
+type FarProc = *const c_void;
+type Handle = *const c_void;
+
+type IcmpCreateFile = extern "stdcall" fn() -> Handle;
+type IcmpSendEcho = extern "stdcall" fn(
+    icmp_handle: Handle,
+    destination_address: IPAddr,
+    request_data: *const u8,
+    request_size: u16,
+    request_options: Option<&IpOptionInformation>,
+    reply_buffer: *mut u8,
+    reply_size: u32,
+    timeout: u32,
+) -> u32;
 
 extern "stdcall" {
     fn LoadLibraryA(name: *const u8) -> HModule;
-    fn GetProcAddress(module: HModule, procName: *const u8) -> FarProc;
+    fn GetProcAddress(module: HModule, proc_name: *const u8) -> FarProc;
 }
 
-type MessageBoxA = extern "stdcall" fn(*const std::ffi::c_void, *const u8, *const u8, u32);
+#[repr(C)]
+struct IPAddr([u8; 4]);
+
+impl fmt::Debug for IPAddr {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let [a, b, c, d] = self.0;
+        write!(f, "{}.{}.{}.{}", a, b, c, d)
+    }
+}
+
+#[repr(C)]
+struct IpOptionInformation {
+    ttl: u8,
+    tos: u8,
+    flags: u8,
+    options_size: u8,
+    options_data: u32,
+}
 
 fn main() {
-    let h = unsafe { LoadLibraryA("USER32.dll\0".as_ptr()) };
-    let MessageBoxA: MessageBoxA =
-        unsafe { std::mem::transmute(GetProcAddress(h, "MessageBoxA\0".as_ptr())) };
+    let ip_hlp_api = unsafe { LoadLibraryA("IPHLPAPI.dll\0".as_ptr()) };
+    let icmp_create_file: IcmpCreateFile =
+        unsafe { mem::transmute(GetProcAddress(ip_hlp_api, "IcmpCreateFile\0".as_ptr())) };
+    let icmp_send_echo: IcmpSendEcho =
+        unsafe { mem::transmute(GetProcAddress(ip_hlp_api, "IcmpSendEcho\0".as_ptr())) };
 
-    MessageBoxA(
-        std::ptr::null(),
-        "Hello from Rust\0".as_ptr(),
-        std::ptr::null(),
-        0,
+    let data = "Foo Bar Baz";
+    let mut reply_buf = vec![0u8; 128];
+
+    let handle = icmp_create_file();
+    let ret = icmp_send_echo(
+        handle,
+        IPAddr([8, 8, 8, 8]),
+        data.as_ptr(),
+        data.len() as u16,
+        Some(&IpOptionInformation {
+            ttl: 128,
+            tos: 0,
+            flags: 0,
+            options_data: 0,
+            options_size: 0,
+        }),
+        reply_buf.as_mut_ptr(),
+        reply_buf.len() as u32,
+        4000,
     );
+
+    println!("ret: {}", ret);
 }
