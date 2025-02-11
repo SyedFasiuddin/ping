9d60c5ef3c6b66464ddaa7ee54321444d7da4592
Syed Fasiuddin <syed.fasiuddin@icloud.com>
2025-02-11 23:50:32 +0530

    use our user level network interface to send ping

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
-mod icmp;
-mod ipv4;
-mod loadlibrary;
+use std::time::Instant;
+use std::time::Duration;
+
+use ersatz::Interface;
+use ersatz::ipv4;
+use ersatz::icmp;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = std::env::args().nth(1).unwrap_or_else(|| {
@@ -8,27 +11,53 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         std::process::exit(1);
     });
 
-    let dest_addr = addr.parse()?;
+    let dest = addr.parse()?;
     let data = "Hello World";
+    let identifier = 0xC0DE;
+    let iface = Interface::open_default()?;
 
-    println!("Pinging {:?} with {} bytes of data:", dest_addr, data.len());
-    for _ in 0..4 {
-        match icmp::Request::new(dest_addr)
-            .ttl(128)
-            .timeout(4000)
-            .data(data)
-            .send()
-        {
-            Ok(res) => println!(
-                "Reply from {:?}: bytes={} time={:?} TTL={}",
-                res.addr,
-                res.data.len(),
-                res.rtt,
-                res.ttl
-            ),
-            Err(_) => println!("Something went wrong"),
+    println!("Pinging {:?} with {} bytes of data:", dest, data.len());
+    for sequence_number in 0..4 {
+        let echo_request = ersatz::icmp::Echo {
+            identifier,
+            sequence_number,
         }
+        .as_echo_request(data)
+        .as_ipv4_payload();
 
+        let before = Instant::now();
+        let rx = iface.expect_ipv4(move |packet| {
+            if let ipv4::Payload::ICMP(ref icmp_packet) = packet.payload {
+                if let icmp::Header::EchoReply(ref reply) = icmp_packet.header {
+                    if reply.identifier == identifier && reply.sequence_number == sequence_number {
+                        return Some((before.elapsed(), packet.clone()));
+                    }
+                }
+            }
+
+            None
+        });
+
+        iface.send_ipv4(echo_request, &dest)?;
+        match rx.recv_timeout(Duration::from_secs(3)) {
+            Ok((elapsed, packet)) => {
+                if let ipv4::Payload::ICMP(ref icmp_packet) = packet.payload {
+                    if let icmp::Header::EchoReply(_) = icmp_packet.header {
+                        println!(
+                            "Reply from {:?}: bytes={} time={:?} TTL={}",
+                            packet.src,
+                            icmp_packet.payload.0.len(),
+                            elapsed,
+                            packet.ttl,
+                        );
+                    }
+                }
+            }
+            Err(_) => {
+                println!("Timed out!");
+                std::process::exit(1);
+            }
+        }
         std::thread::sleep(std::time::Duration::from_secs(1));
     }
 
